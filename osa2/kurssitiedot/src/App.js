f6d94dc449537d46f1c48e004d96af19c2c8ea2b
Jorma Nieminen <donjorgo@gmail.com>
2021-03-28 20:13:53 +0100

    2.5: erillinen moduuli

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,37 +1,5 @@
 import React from 'react'
-
-const Header = (props) => (
-  <h1>{props.course}</h1>
-)
-
-const Part = ({ part }) => (
-  <p>{part.name} {part.exercises}</p>
-)
-
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(part =>
-      <Part key={part.id} part={part} />
-    )}
-  </div>
-)
-
-const Total = ({ parts }) => {
-  const sumExercises = (sum, part) => sum + part.exercises
-  const total = parts.reduce(sumExercises, 0)
-
-  return (
-    <b>Tolal number of exercises {total}</b>
-  )
-}
-
-const Course = ({ course }) =>
-  <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </div>
-
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
