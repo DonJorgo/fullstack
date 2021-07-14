00ca891907eb9124abeffc131024e942d02ada28
Jorma Nieminen <donjorgo@gmail.com>
2021-07-14 14:46:20 +0100

    5.7* blogilistan frontend, step7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,39 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+const Blog = ({blog}) => {
+  const [details, setDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showWithDetails = { display: details ? '' : 'none' }
+
+  const buttonLabel = details ? 'hide' : 'view'
+
+ 
+  const toggleDetails = () => setDetails(!details)
+
+  console.log(blog)
+
+  return(
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <button onClick={toggleDetails}>{buttonLabel}</button>
+      <div style={showWithDetails}>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes}
+          <button>like</button>
+        </div>
+        <div>{blog.user.name}</div>
+      </div>
+
+    </div>
+  )  
+}
 
 export default Blog
