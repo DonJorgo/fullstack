87b0a117bcbd520e3c42b5bc2c4734f788f13f1b
Jorma Nieminen <donjorgo@gmail.com>
2021-07-15 08:21:24 +0100

    fix: inline style for notification

--- osa5/bloglist-frontend/src/components/Notification.js
+++ osa5/bloglist-frontend/src/components/Notification.js
@@ -1,13 +1,31 @@
-import './notification.css'
-
 const Notification = ({ message, isError }) => {
+
+    const NotificationStyle = {
+        background: 'lightgrey',
+        fontSize: '20px',
+        borderStyle: 'solid',
+        borderRadius: '5px',
+        padding: '10px',
+        marginBottom: '10px',
+    }
+
+    const successStyle = {
+        ...NotificationStyle,
+        color: 'green'
+    }
+    
+    const errorStyle = {
+        ...NotificationStyle,
+        color:'red'
+    }         
+
 
     if (message == null) {
         return null
     }
 
     return (
-        <div className={`notification ${isError ? 'error' : 'success'}`}>
+        <div style={isError ? errorStyle : successStyle}>
             {message}
         </div>
     )
