a7e05bc2515614219624e2bf4c3adaa191e72969
Jorma Nieminen <donjorgo@gmail.com>
2021-07-16 19:03:43 +0100

    5.18: blogilistan end to end -testit, step2

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,13 +1,48 @@
 describe('Blog app', function (){
-  this.beforeEach(function () {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Blog App e2e test user',
+      username: 'testuser1',
+      password: 'salasana'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+
     cy.visit('http://localhost:3000')
   })
 
-  it('login form is shown', function() {
+  it('Login form is shown', function() {
     cy.contains('Log in to application')
     cy.contains('username').get('#username')
     cy.contains('password').get('#password')
     cy.contains('button','login')
+  })
+
+  describe('Login', function(){
+    it('succeeds with correct credentials', function(){
+      cy.get('#username').type('testuser1')
+      cy.get('#password').type('salasana')
+      cy.contains('button', 'login').click()
+
+      cy.contains('Blog App e2e test user logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testuser1')
+      cy.get('#password').type('wrongpassword')
+      cy.contains('button', 'login').click()
+
+      cy.contains('invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.contains('Log in to application')
+      cy.contains('username').get('#username')
+      cy.contains('password').get('#password')
+      cy.contains('button','login')
+
+      cy.get('html')
+        .should('not.contain', 'Blog App e2e test user logged in')
+
+    })
   })
 })
