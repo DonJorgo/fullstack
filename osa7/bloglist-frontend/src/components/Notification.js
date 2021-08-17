3a0048637b3908b743efcacabcebde77bf9481d0
Jorma Nieminen <donjorgo@gmail.com>
2021-08-17 19:35:56 +0100

    7.19: tyylit, step1 - Notification

--- osa7/bloglist-frontend/src/components/Notification.js
+++ osa7/bloglist-frontend/src/components/Notification.js
@@ -1,38 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import Alert from 'react-bootstrap/Alert'
 
 const Notification = () => {
 
   const { message, isError } = useSelector(state => state.notification)
-
-  const NotificationStyle = {
-    background: 'lightgrey',
-    fontSize: '20px',
-    borderStyle: 'solid',
-    borderRadius: '5px',
-    padding: '10px',
-    marginBottom: '10px',
-  }
-
-  const successStyle = {
-    ...NotificationStyle,
-    color: 'green'
-  }
-
-  const errorStyle = {
-    ...NotificationStyle,
-    color:'red'
-  }
-
 
   if (message === null) {
     return null
   }
 
   return (
-    <div style={isError ? errorStyle : successStyle}>
+    <Alert variant={isError ? 'danger' : 'success'}>
       {message}
-    </div>
+    </Alert>
   )
 }
 
